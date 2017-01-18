4c8ac2b129c97fa3c9456b25c09b18e7ca564026
Brendan Grubb <brendangrubb@Brendans-MacBook-Pro.local>
2017-01-17 16:52:11 -0800

    populate form, add script to take user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,22 @@ $(document).ready(function() {
 
 
 
+  $("#favorite-things-input form").submit(function(event) {
+    event.preventDefault();
+
+    var favoriteFoodInput = $("input#fav-food").val();
+    var favoritePersonInput = $("input#fav-person").val();
+    var favoritePresidentInput = $("input#fav-president").val();
+
+    $(".fav-food").append(favoriteFoodInput);
+    $(".fav-person").append(favoritePersonInput);
+    $(".fav-president").append(favoritePresidentInput);
+
+
+    $("#user-response").show();
+
+  })
+
 
 
 
